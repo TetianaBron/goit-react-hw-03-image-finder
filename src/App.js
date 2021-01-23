647470f8f3d866010f8d0c4d3bdb4c3f9a50683c
Tetiana Bronitska <tanya.nest87@gmail.com>
2021-01-23 23:22:48 +0200

    Added main

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Layout from './Components/Layout/Layout';
+
+
+
+export default class App extends Component {
+
+    static propTypes = {
+    };
+
+    static defaultProps = {};
+
+    state = {
+    };
+
+   
+
+    render() {
+       
+        return ( 
+            <Layout>
+       
+            </Layout>
+        );
+    }
+}
